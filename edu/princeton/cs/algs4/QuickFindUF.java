840056a9f8e8dedb790087b7958b5ac8e21b5458
thuytien426 <thuytien2710207@gmail.com>
2026-09-13 23:04:35 +0700

    add QuickFindUF

--- edu/princeton/cs/algs4/QuickFindUF.java
+++ edu/princeton/cs/algs4/QuickFindUF.java
@@ -1,7 +1,27 @@
+package princeton.cs.algs4;
+
 public class QuickFindUF {
-    public int [] id;
-    public QuickFindUF 
-    public int find {}
-    public int union {
+    public int[] id;
+
+    public QuickFindUF(int n) {
+        id = new int[n];
+        for (int i = 0; i < n; i++) {
+            id[i] = i;
+        }
+    }
+
+    public int find(int p) {
+        if (p == id[p]) {
+            return p;
+        }
+        return find(id[p]);
+    }
+
+    public void union(int p, int q) {
+        int a = find(p);;
+        int b = find(q);
+        if (a != b) {
+            id[a] = b;
+        }
     }
 }
